c88890ae96c6a7145da13f24e104f2615f74060b
migo <amegahed94@gmail.com>
2021-06-15 16:14:33 -0400

    logging.py
    test

---
+++ logging_sample.py
@@ -0,0 +1,27 @@
+def log_error(error_msg):
+    def query_metadata(endpoint_suffix):
+        import requests
+
+        return requests.get(
+            "http://metadata/computeMetadata/v1/{}".format(endpoint_suffix),
+            headers={"Metadata-Flavor": "Google"},
+        ).text
+
+    from google.cloud import logging
+
+    resource = logging.Resource(
+        type="gce_instance",
+        labels={
+            "project_id": query_metadata("project/project-id"),
+            "instance_id": query_metadata("instance/id"),
+            "zone": query_metadata("instance/zone").split("/")[3],
+        },
+    )
+
+    logger = logging.Client().logger("telephonie-etl-programmes")
+
+    logger.log_text(error_msg, resource=resource, severity="ERROR")
+
+
+log_error("test1")
+log_error("test2")
